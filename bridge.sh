c8acd21a500c2938b7707e7a6705b92a805b3eb7
amaan <aman78shaikh@protonmail.com>
2026-07-01 01:13:09 +0000

    Fixed OS-detection

--- bridge.sh
+++ bridge.sh
@@ -52,7 +52,7 @@ is_ubuntu() {
 }
 
 is_rhel_basedos() {
-  local major_version="${VERSION_ID%%.*}"
+  major_version="${VERSION_ID%%.*}"
   [[ "$ID" =~ ^(almalinux|rocky|centos)$ ]] && [[ "$major_version" -ge 8 ]]
 }
 
@@ -534,7 +534,7 @@ if is_ubuntu; then
     setup_bridge_ubuntu
     ;;
   esac
-elif is_rhel_basedos && [[ "$VERSION_ID" -ge 8 ]]; then
+elif is_rhel_basedos && [[ "$major_version" -ge 8 ]]; then
   case "$PROVIDER" in
   hetzner)
     hetzner_rhel
